1b7b7fd023a724f76faa452118793423df88a6f6
Ethan <zzzh_wh717707@163.com>
2021-08-31 20:25:41 +0800

    second commit

--- src/main/java/com/atguigu/boot/controller/HelloController.java
+++ src/main/java/com/atguigu/boot/controller/HelloController.java
@@ -8,6 +8,6 @@ public class HelloController {
 
     @RequestMapping("/hello")
     public String handle01(){
-        return "hello, Spring Boot 2!";
+        return "hello, Spring Boot 2021!";
     }
 }
